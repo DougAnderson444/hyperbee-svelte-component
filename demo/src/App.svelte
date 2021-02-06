e76f4c4f144b9c7116fa416cccc218c80a688559
Doug <douganderson444@gmail.com>
2021-02-06 17:02:35 -0500

    add svelte demo

---
+++ demo/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from "svelte";
+	// import { default as HyperbeeComponent } from "../../";
+	import HyperbeeComponent from "hyperbee-svelte-component";
+
+	onMount(async () => {});
+</script>
+
+<main>
+	<HyperbeeComponent {feed} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
